0c8ece0d229af61697ed227ca76194a4de83a144
Synthosize <jaydenflynn9@gmail.com>
2026-09-12 21:02:08 -0600

    Load V19 precision Market Finder UI

--- v18-client.js
+++ v18-client.js
@@ -1,3 +1,4 @@
 import('/v19-agency.js').catch(()=>{});
 import('/v19-updates.js').catch(()=>{});
+import('/v19-market.js').catch(()=>{});
 document.documentElement.dataset.siteremadeV18='superseded-by-v19';
