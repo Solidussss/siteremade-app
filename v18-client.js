cc77befbeb950ccf174aab9cf3e2586541e3b25f
Synthosize <jaydenflynn9@gmail.com>
2026-09-13 00:02:55 -0600

    Load lead drawer reliability patch

--- v18-client.js
+++ v18-client.js
@@ -1,4 +1,5 @@
 import('/v19-agency.js').catch(()=>{});
 import('/v19-updates.js').catch(()=>{});
 import('/v19-market.js').catch(()=>{});
+const v29=document.createElement('script');v29.src='/v29-lead-fix.js?v=29';document.head.appendChild(v29);
 document.documentElement.dataset.siteremadeV18='superseded-by-v19';
