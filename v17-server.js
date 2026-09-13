9ac6fdb8da1feac4a16aff1c56df6a35b7ac13e8
Synthosize <jaydenflynn9@gmail.com>
2026-09-13 07:46:38 -0600

    Load native integrations view before app render

--- v17-server.js
+++ v17-server.js
@@ -6,5 +6,6 @@ require('./v29-mail-server');
 require('./v20-loader');
 require('./v19-market-server');
 require('./v30-google-auth');
+require('./v37-static-integrations');
 require('./v17-preload');
 require('./server');
