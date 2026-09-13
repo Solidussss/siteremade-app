b1ba6ae2e91f329d813369dea5fb3917de25303a
Synthosize <jaydenflynn9@gmail.com>
2026-09-13 07:08:47 -0600

    Load integrations hub

--- v29-bootstrap.js
+++ v29-bootstrap.js
@@ -1,1 +1,6 @@
-(()=>{if(!document.querySelector('link[data-v29-mail]')){const l=document.createElement('link');l.rel='stylesheet';l.href='/v29-mail.css?v=43';l.dataset.v29Mail='1';document.head.appendChild(l)}import('/v29-mail-client.js?v=42').catch(()=>{});})();
+(()=>{
+if(!document.querySelector('link[data-v29-mail]')){const l=document.createElement('link');l.rel='stylesheet';l.href='/v29-mail.css?v=43';l.dataset.v29Mail='1';document.head.appendChild(l)}
+if(!document.querySelector('link[data-v34-integrations]')){const l=document.createElement('link');l.rel='stylesheet';l.href='/v34-integrations.css?v=1';l.dataset.v34Integrations='1';document.head.appendChild(l)}
+import('/v29-mail-client.js?v=42').catch(()=>{});
+import('/v34-integrations.js?v=1').catch(()=>{});
+})();
