b905df85562e4dcb563a5a30a804bb7f51b8908f
Synthosize <jaydenflynn9@gmail.com>
2026-09-13 02:29:57 -0600

    Bust Messages Gmail button style cache

--- v29-bootstrap.js
+++ v29-bootstrap.js
@@ -1,1 +1,1 @@
-(()=>{if(!document.querySelector('link[data-v29-mail]')){const l=document.createElement('link');l.rel='stylesheet';l.href='/v29-mail.css?v=36';l.dataset.v29Mail='1';document.head.appendChild(l)}import('/v29-mail-client.js?v=36').catch(()=>{});})();
+(()=>{if(!document.querySelector('link[data-v29-mail]')){const l=document.createElement('link');l.rel='stylesheet';l.href='/v29-mail.css?v=37';l.dataset.v29Mail='1';document.head.appendChild(l)}import('/v29-mail-client.js?v=36').catch(()=>{});})();
