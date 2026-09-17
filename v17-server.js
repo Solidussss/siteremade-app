96aa931e8cfbd53e04295d88da9e65b68c49c063
Synthosize <jaydenflynn9@gmail.com>
2026-09-17 11:48:36 -0600

    Load Google Ads server integration

--- v17-server.js
+++ v17-server.js
@@ -4,6 +4,7 @@ require('./v36-twilio-stripe');
 require('./v39-twilio-provisioning');
 require('./v40-existing-number');
 require('./v34-integrations-server');
+require('./v44-google-ads');
 require('./v29-mail-server');
 require('./v20-loader');
 require('./v19-market-server');
